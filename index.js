5fd6482611deedd3655a4ea94579759db3aca97b
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-02-03 18:01:34 +0100

    feat(WebRCT): Init webRTC connection

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-const WebSocketClient = require('websocket').client;
+const WebSocketClient = require('websocket').client,
+	wrtc = require('wrtc');
 
-var ws = new WebSocketClient();
+let ws = new WebSocketClient(),
+	peerCo,
+	videoChannel;
 
-ws.on('connect', function(connection) {
-	connection.send(JSON.stringify({
+ws.on('connect', function(connec) {
+	connec.on('message', function(message) {
+		if (message.type === 'utf8') {
+			let json = JSON.parse(message.utf8Data);
+
+			processSocketMessage(json, connec);
+		}
+	});
+
+	connec.send(JSON.stringify({
 		type: 'DRONE',
 		action: 'INIT_SOCKET',
 		name: 'Drone 001'
 	}));
 });
+
+const processSocketMessage = function (json, connec) {
+	switch (json.action) {
+		case 'INIT_PEER_CO':
+			var desc = new wrtc.RTCSessionDescription(json.localDescription);
+			peerCo = new wrtc.RTCPeerConnection();
+
+			peerCo.onicecandidate = function (event) {
+				console.info('Client on ICE candidate', event);
+
+				if (event.candidate) {
+					connec.send(JSON.stringify(Object.assign(json, {
+						type: 'DRONE',
+						action: 'RTC_ICE_CANDIDATE',
+						candidate: event.candidate
+					})));
+				}
+			};
+
+			peerCo.setRemoteDescription(desc)
+				.then(function() {
+					console.log('Create WebRTC answer.');
+					return peerCo.createAnswer();
+				}).then(answerDesc => {
+					console.info('Set WebRTC desc from answer: ', answerDesc);
+					return peerCo.setLocalDescription(answerDesc);
+				}).then(() => {
+					console.info('Set server remote desc from client answer');
+					connec.send(JSON.stringify(Object.assign(json, {
+						localDescription: pc.localDescription
+					})));
+				});
+
+			break;
+		case 'RTC_ICE_CANDIDATE':
+			console.log('RTC_ICE_CANDIDATE');
+			break;
+		default:
+			console.error('Undefined action...');
+	}
+};
 
 ws.connect('ws://localhost:3000/', 'echo-protocol');
